8d85e565dd70c4c031fe605c20f60a32cdc3225d
nikitacheba <nikitacheba.94@yandex.ru>
2020-05-20 13:44:56 +0300

    Initial commit - project start

---
+++ movies/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect
+from .models import Movie, Category, Actor
+from django.views.generic.base import View
+from django.views.generic import ListView, DetailView
+from .forms import ReviewForm
+
+class MoviesView(ListView):
+	"""Список фильмов"""
+	model = Movie
+	queryset = Movie.objects.filter(draft=False)
+	
+	
+	
+class MovieDetailView(DetailView):
+	"""Полное описание фильма"""
+	model = Movie
+	slug_field = "url"
+
+class AddReview(View):
+	"""Отзывы"""
+	def post(self, request, pk):
+		form = ReviewForm(request.POST)
+		movie = Movie.objects.get(id=pk)
+		if form.is_valid():
+			form = form.save(commit=False)
+			if request.POST.get("parent", None):
+				form.parent_id = int(request.POST.get("parent"))
+			form.movie = movie
+			form.save()
+		return redirect(movie.get_absolute_url())
+
+
+class ActorView(DetailView):
+	"""Вывод информации об актере"""
+	model = Actor
+	template_name = "movies/actor.html"
+	slug_field = "name"
